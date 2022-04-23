c57a89a5aec3bb61c6c5b9a6cd945c15b3115ad3
Windows <Windows@DESKTOP-LQGB61O>
2022-04-23 11:49:30 +0200

    versione1

---
+++ GitHubProva1/src/Principale.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @author Parisi
+ * 
+ * @version 1.0
+ *
+ */
+public class Principale {
+
+	public static void main(String[] args) {
+		System.out.println("prova GitHub 1");
+
+	}
+
+}
